512e16267262fdadd3735299f4423cf6fec783a1
MariaB <mbintanel@gmail.com>
2026-06-05 10:20:05 +0200

    primera version

---
+++ includes/menu.php
@@ -0,0 +1,13 @@
+<nav>
+
+    <ul>
+
+        <li><a href=<?=  $vista1??"index.php"?>>Inici</a></li>
+
+        <li><a href=<?= $vista1??"views/destinacions.php"?>>Destinacions</a></li>
+
+        <li><a href=<?= $vista3??"views/contacte.php"?>>Contacte</a></li>
+
+    </ul>
+
+</nav>
